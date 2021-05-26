0220fc1080c33f24d352149b14aea25ff7d9dec2
deltagare <jantool@icloud.com>
2021-05-26 13:51:06 +0200

    Define new field

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,5 +10,33 @@ public class App
     {
         System.out.println( "Hello World!" );
         int num1=10;
+        System.out.println("Bye Buddy!");
+
+        int Speed =100;
+
+        System.out.println("Speed");
+
+        String myName = "Erik";
+        System.out.println(myName);
+
+        String firstName = "Jaime";
+        String lastName = "Torrealba";
+        System.out.println(firstName + " " + lastName);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
     }
-}
+
